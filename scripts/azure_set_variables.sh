174eecdaed2b3d2600e934f85a144e13cf048f51
Hugo Meiland <humeilan@microsoft.com>
2020-11-06 07:13:24 +0100

    add specific sku

--- scripts/azure_set_variables.sh
+++ scripts/azure_set_variables.sh
@@ -2,6 +2,20 @@
 
 mkdir -p /var/lib/azurehpc/
 
-SKU=$(curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2020-06-01" | jq -r .compute.vmSize)
+META_SKU=$(curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2020-06-01" | jq -r .compute.vmSize)
+NVME=false
+
+case "$META_SKU" in
+  Standard_HC44rs)
+    SKU=hc44rs
+    ;;
+  Standard_HB120rs_v2)
+    SKU=hb120rsv2
+    NVME=true
+    ;;
+  *)
+    SKU=generic
+    ;;
 
 echo "export AZURE_SKU=${SKU}" > /var/lib/azurehpc/azure_variables.sh
+echo "export AZURE_NVME=${NVME}" >> /var/lib/azurehpc/azure_variables.sh
